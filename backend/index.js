9afaadc845a7fad7d48dfc42c986dcbc08675b47
Rakesh Pal <rkpal8097@gmail.com>
2023-12-04 11:11:30 +0530

    final_cors_changes1

--- backend/index.js
+++ backend/index.js
@@ -3,22 +3,26 @@ const http = require("http");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const server = http.createServer(app);
+// const server = http.createServer(app);
 const mongoDb = require("./db");
 
 // cors enabling access
-
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://react-food-delivery-app.netlify.app/"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept"
-  );
-  next();
-});
+var corsOptions = {
+  origin: "https://react-food-delivery-app.netlify.app/",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://react-food-delivery-app.netlify.app/"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-Requested-With,Content-Type,Accept"
+//   );
+//   next();
+// });
 app.use(express.json());
 
 mongoDb();
@@ -27,9 +31,9 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/api", require("./Routes/CreateUser"));
-app.use("/api", require("./Routes/DisplayData"));
-app.use("/api", require("./Routes/OrderData"));
-server.listen(port, () => {
+app.use("/api", cors(corsOptions), require("./Routes/CreateUser"));
+app.use("/api", cors(corsOptions), require("./Routes/DisplayData"));
+app.use("/api", cors(corsOptions), require("./Routes/OrderData"));
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
