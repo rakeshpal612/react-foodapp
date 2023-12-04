84f80c546c3b759a4de7a602d9c046b2591b323a
Rakesh Pal <rkpal8097@gmail.com>
2023-12-04 11:04:35 +0530

    final_cors_changes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const app = express();
-
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const mongoDb = require("./db");
@@ -9,7 +9,10 @@ const mongoDb = require("./db");
 // cors enabling access
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://react-food-delivery-app.netlify.app/"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept"
